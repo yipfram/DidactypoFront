668c91be9512c9da5f79ae03d8867b335cb0ff3e
Xavier LACROIX <xavier.stoa@gmail.com>
2024-09-18 10:11:06 +0200

    Ajout Header footer

---
+++ src/pages/Accueil.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Accueil() {
+ return (
+    <>
+        <h1>
+            Titre
+        </h1>
+        <p>
+            Salut la team
+        </p>
+    </>
+ )
+}
