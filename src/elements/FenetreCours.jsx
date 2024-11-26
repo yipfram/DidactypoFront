e3f320f9ad37d27177e79915cc8ad7e366d84169
denis <denis.koehler@csilyon.fr>
2024-11-26 15:37:37 +0100

    changement H+F

--- src/elements/FenetreCours.jsx
+++ src/elements/FenetreCours.jsx
@@ -1,19 +1,21 @@
 import style from "../style/Apprendre.module.css";
 import { useState } from "react";
 import Cours from "./Cours";
+import ListeCours from "../pages/ListeCours";
 
-export default function FenetreCours(idCours){
+export default function FenetreCours(props){
     const [index,setIndex] = useState(0); 
     const [cours,setCours] = useState(genererCours());
     var partieCours = cours[index];
 
+    const setShowCours = props.setShowCours();
 
     /**
      * fonction qui retourne un tableau contenant toutes les parties d'un cours, donc maximum une image par partie de cours
      */
     function genererCours(){
         //tableau avec p et lien vers image
-        /*let cours = Cours(idCours);
+        /*let cours = Cours(props.idCours);
         let partiesCours = cours.split(";");
         return partiesCours; */
         return ["Première page","Deuxième page"];
@@ -28,16 +30,17 @@ export default function FenetreCours(idCours){
         if(index > 0)
             setIndex(index-1);
     }
-    
 
 
     return(
         <>
             <div className={style.fenetreCours}>
                 <p>{partieCours}</p>
-                
-                <button onClick={HandlerBack}> back </button>
-                <button onClick={HandlerNext}> next </button>
+                <div className = {style.groupeButtonFenetreCours}>
+                    <button onClick={HandlerBack}> back </button>
+                    <button onClick={props.setShowCours(false)}>exit</button>
+                    <button onClick={HandlerNext}> next </button>
+                </div>
             </div>
         </>
 
