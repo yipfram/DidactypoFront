4af9bb5a0cbf2ecbde02389179f613ee9c0f049d
denis <denis.koehler@csilyon.fr>
2024-12-05 16:28:50 +0100

    lein entre bd et sous_cours fait, manque tri par id_sous_cours

--- src/elements/FenetreCours.jsx
+++ src/elements/FenetreCours.jsx
@@ -20,7 +20,12 @@ export default function FenetreCours(props){
     const genererCours = async() =>{
         try {
             const resultat = await api.get(`/sous_cours/${idCours}`); //on veut récupérer tous les souscours d'un cours
-            setCours(resultat);
+            const data = resultat.data
+            /* La table sous_cours a comme attributs : id_sous_cours,id_cours_parent, titre_sous_cours, contenu_cours, 
+            * chemin_img_sous_cours. La clé primaire est sur les deux id
+            * On utilise id_sous_cours pour définir l'ordre des sous cours
+            */
+            setCours(data);
         } catch (error) {
             console.error("Erreur lors de la récupération d'une partie d'un cours :", error );
         }
@@ -46,12 +51,30 @@ export default function FenetreCours(props){
         });
     }
 
+    function contenuPartieCours(){
+        return(
+            <div className={style.contenuPartieCours}>
+                    {partieCours ? ( //il faut faire ça à cause du delai de la requête bd, sinon il y a ReferenceError
+                        <>
+                        {/*Tests pour afficher un élément seulement si on l'a définit dans la bd
+                        *On peut donc combiner les composants, par exemple seulement une image ou un titre et un paragraphe*/}
+
+                        {partieCours.titre_sous_cours && <h1>{partieCours.titre_sous_cours}</h1>}
+                        {partieCours.contenu_cours && <p>{partieCours.contenu_cours}</p>}
+                        {partieCours.chemin_img_sous_cours && <img src={partieCours.chemin_img_sous_cours} alt="image partie cours"></img>}
+                        
+                        </>
+                    ) : (
+                        <p>Chargement...</p>
+                    )}
+                </div>
+        );
+    }
+
     return(
         <>
             <div className={style.fenetreCours}>
-                <div className={style.contenuPartieCours}>
-                    <p>{partieCours}</p>
-                </div>
+                {contenuPartieCours()}
                 <div className = {style.groupeButtonFenetreCours}>
                     <button onClick={handlerBack}> back </button>
                     <button onClick={handlerExit}>exit</button> {/*setShowCours cache la FenetreCours */}
