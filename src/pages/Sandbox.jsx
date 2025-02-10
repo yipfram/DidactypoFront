5ce497bd590e251173801b1f00956fb33ad9e1a5
Xavi <xavier.stoa@gmail.com>
2025-02-10 13:28:26 +0100

    Revert "CSS sandbox, pret pour merge"
    This reverts commit 97f6ff15d8a9fca1b658fe17eeac04d56eaa1c83.

--- src/pages/Sandbox.jsx
+++ src/pages/Sandbox.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import InterfaceSaisie from "../elements/InterfaceSaisie/InterfaceSaisie.jsx";
 
-import style from "../style/Sandbox.module.css";
-
 export default function Sandbox() {
     const [customText, setCustomText] = useState("");
     const [targetText, setTargetText] = useState("");
@@ -18,29 +16,23 @@ export default function Sandbox() {
     return (
         <main style={{ textAlign: "center", padding: "20px" }}>
             {!isReady ? (
-                <div className={style.sandbox}>
-                    <h2 className={style.titreGeneral}>Mode Sandbox</h2>
-                    <h3 className={style.sousTitre}>
-                        Bienvenue dans le mode Sandbox (Bac à sable), ici tu vas pouvoir t'entrainer a taper rapidement, ou sur ta technique.<br />
-                        Tu peux écrire ton propre texte et appuyer sur le petit bouton pour démarrer l'exercice. Bonne chance !
-                    </h3>
+                <div className="sandbox">
+                    <h2>Mode Sandbox</h2>
                     <textarea
                         placeholder="Entrez votre texte ici..."
                         value={customText}
                         onChange={(e) => setCustomText(e.target.value)}
                         rows={3}
                         cols={50}
-                        style={{ fontSize: "16px", padding: "5px", marginTop: "4vh" }}
+                        style={{ fontSize: "16px", padding: "5px" }}
                     />
                     <br />
-                    <button onClick={handleStart}>
+                    <button onClick={handleStart} style={{ marginTop: "10px", padding: "8px 15px", fontSize: "16px" }}>
                         Démarrer l'exercice
                     </button>
                 </div>
             ) : (
-                <div className={style.InterfaceSaisie}>
                 <InterfaceSaisie targetText={targetText} isReady={isReady} />
-                </div>
             )}
         </main>
     );
