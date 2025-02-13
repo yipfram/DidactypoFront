9c0854678946308b6d2641697a56ed4e6b6dfceb
Romain ROCHAS <romainrochas69@gmail.com>
2025-02-13 18:27:16 +0200

    Secure firebase credentials

--- src/firebase-config.jsx
+++ src/firebase-config.jsx
@@ -1,19 +1,47 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import dotenv from 'dotenv';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Check if all necessary environment variables are set
+const requiredEnvVars = [
+  'FIREBASE_API_KEY',
+  'FIREBASE_BASE_URL',
+  'FIREBASE_PROJECT_ID',
+  'FIREBASE_STORAGE_BUCKET',
+  'FIREBASE_MESSAGING_SENDER_ID',
+  'FIREBASE_APP_ID'
+];
+
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDLYfJAz2X1uRgsrvp9d_Nh3YbhqlVlMY0",
-  authDomain: "didactypo.firebaseapp.com",
-  projectId: "didactypo",
-  storageBucket: "didactypo.firebasestorage.app",
-  messagingSenderId: "883369472393",
-  appId: "1:883369472393:web:e9a32e7690a0de595fb158"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_BASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = () => {
+  for (const envVar of requiredEnvVars) {
+    if (!process.env[envVar]) {
+      console.error(`Missing environment variable: ${envVar}`);
+      process.exit(1);
+      // Check the console :)
+    }
+  }
+  return initializeApp(firebaseConfig);
+}
 export const db = getFirestore(app);
